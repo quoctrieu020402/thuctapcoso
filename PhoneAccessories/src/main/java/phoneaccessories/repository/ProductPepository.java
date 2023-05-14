caf29577ef1c86cb1536f0e5807ff8b48d536b23
voquocdinh <96035704+VQuocDinh@users.noreply.github.com>
2023-05-14 15:41:05 +0700

    Delete PhoneAccessories directory

--- PhoneAccessories/src/main/java/phoneaccessories/repository/ProductPepository.java
+++
@@ -1,9 +0,0 @@
-package phoneaccessories.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import phoneaccessories.entity.Product;
-
-public interface ProductPepository extends JpaRepository<Product, String> {
-
-}
